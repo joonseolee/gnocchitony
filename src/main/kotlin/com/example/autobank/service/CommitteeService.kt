1efbf4c3aca355ab9a5e4db7a0b5db6b8bdfb7fa
linnerudsander <sandesl@stud.ntnu.no>
2024-02-15 15:44:57 +0100

    #11 fixed typo

--- src/main/kotlin/com/example/autobank/service/CommitteeService.kt
+++ src/main/kotlin/com/example/autobank/service/CommitteeService.kt
@@ -6,10 +6,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @Service
 class CommitteeService(val db: JdbcTemplate) {
-    fun findCommittees(): List<Comitee> = db.query("select * from committee") { response, _ ->
+    fun findCommittees(): List<Comitee> = db.query("select * from comitee") { response, _ ->
             Comitee(response.getInt("id"), response.getString("name"))
     }
 
-    fun addCommittee(name: String) = db.update("insert into committee (name) VALUES ('${name}')");
+    fun addCommittee(name: String) = db.update("insert into comitee (name) VALUES ('${name}')");
 
     }
