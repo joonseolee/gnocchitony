26d730c69165d50b2a5a6f1e530d30cf50824fd4
dinamst <dinamst@stud.ntnu.no>
2024-02-18 16:46:00 +0100

    #15 chack if user is admin

--- src/main/kotlin/com/example/autobank/controller/Admin.kt
+++ src/main/kotlin/com/example/autobank/controller/Admin.kt
@@ -1,10 +1,33 @@
 package com.example.autobank.controller
 
+import com.example.autobank.data.User
+import com.example.autobank.service.AdminService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/admin")
-class Admin() {
+class Admin(@Autowired val adminService: AdminService) {
+    @GetMapping("/all")
+    fun getAdmins(): ResponseEntity<Any>{
+        val admins = adminService.getAdmin()
+        return ResponseEntity.ok(admins)
+
+    }
+
+    @GetMapping("/login")
+    fun checkAdmin(user: User): Boolean {
+        val admins = adminService.getAdmin()
+        for (admin in admins){
+            if (user.id == admin.id){
+                return true
+            }
+        }
+        return false
+    }
 
 }
--- src/main/kotlin/com/example/autobank/service/AdminService.kt
+++ src/main/kotlin/com/example/autobank/service/AdminService.kt
@@ -10,7 +10,11 @@ import java.util.List;
 
 @Service
 public class AdminService(val db: JdbcTemplate) {
-    fun getAdmin(): List<User>{
-        return db.query("SELECT * FROM onlineuser WHERE admin=1");
+    fun getAdmin(): MutableList<User> {
+        return db.query("SELECT * FROM onlineuser WHERE admin=1"){
+            response, _ -> User(response.getInt("id"), response.getString("email"), response.getString("online_id"), response.getBoolean("admin"))
+        }
     }
+    
+
 }
